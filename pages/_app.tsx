2c07c9646409d5a3f8989fddcdc26a8fca4a85f9
HARSHH PRASAD <harshprasadedu@gmail.com>
2024-02-07 13:02:28 +0530

    initial ui and authentication using graphQL

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Toaster } from "react-hot-toast";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <div className={inter.className} >
+    <GoogleOAuthProvider clientId="404903219917-um4re238g4hh2fk26najccqr47fargnr.apps.googleusercontent.com">
+      <Component {...pageProps} />
+      <Toaster></Toaster>
+    </GoogleOAuthProvider>
+  </div>
 }
